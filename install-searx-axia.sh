871171011d290020bc550324e021ac738b755814
Alexander Steffen <alex@ajhs.legal>
2020-11-10 17:41:37 -0500

    fixed permission issue
    not a good way of doing it tho

--- install-searx-axia.sh
+++ install-searx-axia.sh
@@ -1,3 +1,7 @@
 #/bin/bash
-echo "Follow the prompts\n"
+echo "Follow the prompts \n"
+cd /home
+git clone https://github.com/djdynam/Axia axia
+chmod -R 777 axia 
+cd axia
 bash utils/searx.sh install all
